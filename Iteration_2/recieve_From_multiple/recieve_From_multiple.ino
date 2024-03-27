4f48236435b33760d948f5fd9313d291814d439c
adamp043 <adam.abdulahad@mail.utoronto.ca>
2024-03-26 20:43:14 -0400

    Debugged receiever code

--- Iteration_2/recieve_From_multiple/recieve_From_multiple.ino
+++ Iteration_2/recieve_From_multiple/recieve_From_multiple.ino
@@ -17,8 +17,8 @@ const uint64_t sensor1 = 0xF0F0F0F0AA;
 const uint64_t sensor2 = 0xF0F0F0F066;
 
 struct data_packet {
-  int moistureValue;
-  int sensorNum;
+  uint16_t moistureValue;
+  uint16_t sensorNum;
 };
 data_packet transmitted_data;
 void setup() {
@@ -29,15 +29,14 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_250KBPS);
   radio.startListening();
-  transmitted_data.sensorNum = 5;
 }
 
 void loop() {
 
   if (radio.available()) {
     radio.read(&transmitted_data, sizeof(transmitted_data));
-    Serial.println(transmitted_data.sensorNum);
-    //Serial.print("Sensor: "); Serial.print(transmitted_data.sensorNum); Serial.print(" | Moisture Value: "); Serial.println(transmitted_data.moistureValue);
+    //Serial.println(transmitted_data.sensorNum);
+    Serial.print("Sensor: "); Serial.print(transmitted_data.sensorNum); Serial.print(" | Moisture Value: "); Serial.println(transmitted_data.moistureValue);
 
     //radio.read(&transmitted_data, sizeof(transmitted_data));
     //Serial.print("Sensor: "); Serial.print(transmitted_data.sensorNum); Serial.print(" | Moisture Value: "); Serial.println(transmitted_data.moistureValue);
